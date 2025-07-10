b97f74711f77f75fb511ef6541b730b944df70de
Jyothi <jyothiveeranala3@gmail.com>
2025-07-10 18:58:52 +0530

    vpc-module-test

---
+++ main.tf
@@ -0,0 +1,173 @@
+#vpc creation
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  instance_tenancy = "default" #one vpc have many ec2 instance
+#I.T defines whether your EC2 instances share the underlying hardware with other customers (shared tenancy) or run on dedicated hardware (dedicated tenancy)
+  
+  # expense-dev
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+
+#aws_internet_gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags,
+    {
+      Name = local.resource_name
+    }
+  )
+}
+
+#aws_subnet
+# expense-dev-public-us-east-1a
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true #mainroute attach to publics only
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnet_tags,
+    {
+      Name = "${local.resource_name}-public-${local.az_names[count.index]}"
+    }
+  )
+}
+
+# expense-dev-private-us-east-1a
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.private_subnet_tags,
+    {
+      Name = "${local.resource_name}-private-${local.az_names[count.index]}"
+    }
+  )
+}
+
+# expense-dev-database-us-east-1a
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_tags,
+    {
+      Name = "${local.resource_name}-database-${local.az_names[count.index]}"
+    }
+  )
+}
+
+#elastic IP
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+#NAt gateway
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags,
+    var.nat_gateway_tags,
+    {
+      Name = local.resource_name
+    }
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+#Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.public_route_table_tags,
+    {
+      Name = "${local.resource_name}-public"
+    }
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.private_route_table_tags,
+    {
+      Name = "${local.resource_name}-private"
+    }
+  )
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.database_route_table_tags,
+    {
+      Name = "${local.resource_name}-database"
+    }
+  )
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.example.id
+}
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.example.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
